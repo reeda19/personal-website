b982627d8414d633aec907faca2e4ce4168d7f11
Alex Reed <alexreed100@gmail.com>
2021-03-15 19:02:00 -0400

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import { Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="#home">Alex Reed</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">About</Nav.Link>
+            <NavDropdown title="Projects" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Bar Calculator</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Movie Database</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Pyramid Solitaire</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
