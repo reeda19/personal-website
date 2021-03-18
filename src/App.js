b136719f586aa14d3ccdd1abdf88c0f0c2b183fa
Alex Reed <alexreed100@gmail.com>
2021-03-18 18:11:41 -0400

    set up basic routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
+import React from "react";
 import { Nav, Navbar, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>    <div className="App">
       <Navbar bg="dark" variant="dark" expand="lg">
         <Navbar.Brand href="#home">Alex Reed</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">About</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
             <NavDropdown title="Projects" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Bar Calculator</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Movie Database</NavDropdown.Item>
@@ -24,8 +31,41 @@ function App() {
         </Navbar.Collapse>
       </Navbar>
       <header className="App-header">
-
+      <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <p>test</p>
+          </Route>
+        </Switch>
       </header>
+    </div>
+
+
+
+
+    </Router>
+
+  );
+}
+
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
     </div>
   );
 }
